c10cd320ab4d89dd388accd8d783fba5be47df6e
Pr1n <pr1n@qq.com>
2021-03-05 11:45:48 +0800

    Update Count.

--- app/src/main/java/com/example/count/MainActivity.kt
+++ app/src/main/java/com/example/count/MainActivity.kt
@@ -18,6 +18,7 @@ package com.example.count
 import android.app.Application
 import android.content.ContextWrapper
 import android.graphics.Point
+import android.graphics.PointF
 import android.os.Bundle
 import android.view.MotionEvent
 import androidx.activity.compose.setContent
@@ -44,7 +45,8 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.input.pointer.pointerInteropFilter
 import androidx.compose.ui.layout.Layout
 import androidx.compose.ui.layout.layout
@@ -83,13 +85,15 @@ class MainActivity : AppCompatActivity() {
     }
 }
 
+val statusHeight by lazy {
+    val resources = AppContext.resources
+    val resourceId = resources.getIdentifier("status_bar_height", "dimen", "android")
+    resources.getDimensionPixelSize(resourceId)
+}
+
 // Start building your app here!
 @Composable
 fun MyApp() {
-    val resources = AppContext.resources
-    val resourceId = resources.getIdentifier("status_bar_height", "dimen", "android")
-    val statusHeight = resources.getDimensionPixelSize(resourceId)
-
     var centerX = 0
     var centerY = 0
     var touchX: Int
@@ -99,6 +103,8 @@ fun MyApp() {
     var currentAngle by rememberSaveable { mutableStateOf(0F) }
 
     var contentAnim by remember { mutableStateOf(0F) }
+
+    var rotation by remember { mutableStateOf(PointF(0F, 0F)) }
 
     var countDownAnimEnable by remember { mutableStateOf(false) }
     var countDownAnim by remember { mutableStateOf(0F) }
@@ -129,93 +135,133 @@ fun MyApp() {
                 }
             }
     ) {
-        Text(
-            text = dateTimeText,
-            modifier = Modifier
-                .padding()
-                .align(Alignment.TopCenter)
-                .padding(top = 50.dp),
-            style = MaterialTheme.typography.h2
-        )
-
-        Layout(
-            content = {
-                Image(
-                    painter = painterResource(id = R.mipmap.block),
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .rotate(if (countDownAnimation != 0F) countDownAnimation else contentAnim),
-                    contentDescription = "block"
-                )
-            },
-            modifier = Modifier
-                .size(300.dp)
-                .align(Alignment.Center)
-                .background(MaterialTheme.colors.primary, CircleShape)
-                .pointerInteropFilter {
-                    when (it.action) {
-                        MotionEvent.ACTION_DOWN -> {
-                            touchX = it.rawX.toInt()
-                            touchY = it.rawY.toInt() - statusHeight
-                            val angle = getAngle(Point(touchX, touchY), Point(centerX, centerY))
-                            offsetAngle = angle.toFloat()
-                            !countDownAnimEnable
-                        }
-                        MotionEvent.ACTION_MOVE -> {
-                            touchX = it.rawX.toInt()
-                            touchY = it.rawY.toInt() - statusHeight
-                            val angle = getAngle(Point(touchX, touchY), Point(centerX, centerY))
-                            val tmp = currentAngle + angle.toFloat() - offsetAngle
-                            contentAnim = if (tmp < 0) 360 + tmp else tmp
-                            true
-                        }
-                        MotionEvent.ACTION_UP, MotionEvent.ACTION_CANCEL -> {
-                            touchX = it.rawX.toInt()
-                            touchY = it.rawY.toInt() - statusHeight
-                            val angle = getAngle(Point(touchX, touchY), Point(centerX, centerY))
-                            currentAngle = currentAngle + angle.toFloat() - offsetAngle
-                            composableScope.launch(Dispatchers.IO) {
-                                countDownAnimEnable = true
-                                val dataTimeFlow = dataTimeFlow(dateTime)
-                                dataTimeFlow.collect { dt ->
-                                    if (countDownAnimEnable) dateTimeText = "$dt"
-                                    val tmp = dt / 60F * 360
-                                    if (dt == 0) {
-                                        countDownAnimEnable = false
-                                        offsetAngle = 0F
-                                        currentAngle = 0F
-                                        contentAnim = 0F
-                                    }
-                                    countDownAnim = tmp
-                                }
-                            }
-                            false
-                        }
-                        else -> false
+        val modifier = Modifier
+            .size(300.dp)
+            .padding(16.dp)
+            .align(Alignment.Center)
+            .shadow(
+                elevation = if (rotation.x == 0F && rotation.y == 0F) 5.dp else 0.dp,
+                shape = CircleShape,
+                clip = true
+            )
+            .graphicsLayer {
+                rotationX = rotation.x
+                rotationY = rotation.y
+                scaleX = if (rotation.x == 0F && rotation.y == 0F) 1F else 0.98F
+                scaleY = if (rotation.x == 0F && rotation.y == 0F) 1F else 0.98F
+                rotationZ = if (countDownAnimation != 0F) countDownAnimation else contentAnim
+            }
+            .pointerInteropFilter {
+                when (it.action) {
+                    MotionEvent.ACTION_DOWN -> {
+                        touchX = it.rawX.toInt()
+                        touchY = it.rawY.toInt() - statusHeight
+                        val angle = getAngle(Point(touchX, touchY), Point(centerX, centerY))
+                        offsetAngle = angle.toFloat()
+                        if (!countDownAnimEnable) rotation = getRotation(angle)
+                        !countDownAnimEnable
                     }
-                },
-            measurePolicy = { measurables, constraints ->
-                val placeables = measurables.map { it.measure(constraints = constraints) }
-                layout(constraints.maxWidth, constraints.maxHeight) {
-                    placeables.forEach { it.placeRelative(0, 0) }
+                    MotionEvent.ACTION_MOVE -> {
+                        touchX = it.rawX.toInt()
+                        touchY = it.rawY.toInt() - statusHeight
+                        val angle = getAngle(Point(touchX, touchY), Point(centerX, centerY))
+                        rotation = getRotation(angle)
+                        val tmp = currentAngle + angle.toFloat() - offsetAngle
+                        contentAnim = if (tmp < 0) 360 + tmp else tmp
+                        true
+                    }
+                    MotionEvent.ACTION_UP, MotionEvent.ACTION_CANCEL -> {
+                        touchX = it.rawX.toInt()
+                        touchY = it.rawY.toInt() - statusHeight
+                        val angle = getAngle(Point(touchX, touchY), Point(centerX, centerY))
+                        rotation = PointF(0F, 0F)
+                        currentAngle = currentAngle + angle.toFloat() - offsetAngle
+                        composableScope.launch(Dispatchers.IO) {
+                            countDownAnimEnable = true
+                            val dataTimeFlow = dataTimeFlow(dateTime)
+                            dataTimeFlow.collect { dt ->
+                                if (countDownAnimEnable) dateTimeText = "$dt"
+                                val tmp = dt / 60F * 360
+                                if (dt == 0) {
+                                    countDownAnimEnable = false
+                                    offsetAngle = 0F
+                                    currentAngle = 0F
+                                    contentAnim = 0F
+                                }
+                                countDownAnim = tmp
+                            }
+                        }
+                        false
+                    }
+                    else -> false
                 }
             }
+
+        Content(
+            modifier = modifier,
+            dateTimeText = dateTimeText,
+            textModifer = Modifier.align(Alignment.TopCenter)
         )
     }
 }
 
+@Composable
+fun Content(modifier: Modifier, dateTimeText: String, textModifer: Modifier) {
+    Text(
+        text = dateTimeText,
+        modifier = textModifer
+            .padding()
+            .padding(top = 50.dp),
+        style = MaterialTheme.typography.h2
+    )
+
+    Layout(
+        content = {
+            Image(
+                painter = painterResource(id = R.mipmap.clock),
+                modifier = Modifier.fillMaxSize(),
+                contentDescription = "block"
+            )
+        },
+        modifier = modifier,
+        measurePolicy = { measurables, constraints ->
+            val placeables = measurables.map { it.measure(constraints = constraints) }
+            layout(constraints.maxWidth, constraints.maxHeight) {
+                placeables.forEach { it.placeRelative(0, 0) }
+            }
+        }
+    )
+}
+
 fun getAngle(touchPoint: Point, centerPoint: Point): Double {
-    // 两点在X轴的距离
     val lenX = (touchPoint.x - centerPoint.x)
-    // 两点在Y轴距离
     val lenY = (touchPoint.y - centerPoint.y)
-    // 两点距离
     val lenXY = sqrt((lenX * lenX + lenY * lenY).toDouble()).toFloat()
-    // 计算弧度
     val radian = acos((lenX / lenXY).toDouble()) * if (touchPoint.y < centerPoint.y) -1 else 1
     val tmp = (radian / Math.PI * 180).roundToInt().toDouble()
-    // 计算角度
     return ((if (tmp >= 0) tmp else 360 + tmp) + 90) % 360
+}
+
+fun getRotation(angle: Double, offset: Float = 10F): PointF {
+    return when (val temp = angle % 360) {
+        in 0.0..90.0 -> {
+            val tempAngle = (temp - 0) / 90
+            PointF((offset - tempAngle * offset).toFloat(), (tempAngle * offset).toFloat())
+        }
+        in 90.0..180.0 -> {
+            val tempAngle = (temp - 90) / 90
+            PointF(-(tempAngle * offset).toFloat(), (offset - tempAngle * offset).toFloat())
+        }
+        in 180.0..270.0 -> {
+            val tempAngle = (temp - 180) / 90
+            PointF(-(offset - tempAngle * offset).toFloat(), -(tempAngle * offset).toFloat())
+        }
+        in 270.0..360.0 -> {
+            val tempAngle = (temp - 270) / 90
+            PointF((tempAngle * offset).toFloat(), -(offset - tempAngle * offset).toFloat())
+        }
+        else -> PointF(0F, 0F)
+    }
 }
 
 suspend fun dataTimeFlow(dateTime: Int) =
